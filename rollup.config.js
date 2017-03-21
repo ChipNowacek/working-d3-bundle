56e42c38bcea75072669861c91fa25866a51d39f
Mike Bostock <mbostock@gmail.com>
2017-03-21 04:42:21 -0700

    Update dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,5 @@
-import npm from "rollup-plugin-node-resolve";
+import node from "rollup-plugin-node-resolve";
 
 export default {
-  entry: "index.js",
-  format: "umd",
-  moduleName: "d3",
-  plugins: [npm({jsnext: true})],
-  dest: "d3.js"
+  plugins: [node()]
 };
